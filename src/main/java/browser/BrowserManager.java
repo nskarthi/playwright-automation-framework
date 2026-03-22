eb8f1879e3af9858e354c33cf202c4be9b28152d
Karthikeyan Selvaraj <nskarthi@gmail.com>
2026-03-22 09:06:19 -0400

    fixes and new changes

--- src/main/java/browser/BrowserManager.java
+++ src/main/java/browser/BrowserManager.java
@@ -6,6 +6,14 @@ import com.microsoft.playwright.*;
 
 
 import java.awt.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class BrowserManager {
     public Playwright playwright;
@@ -13,34 +21,60 @@ public class BrowserManager {
     public BrowserContext browserContext;
     public Page page;
     public Page.WaitForSelectorOptions options;
+    public Properties properties;
+
+    private static final Logger logger = Logger.getLogger(BrowserManager.class.getName());
+
+    public BrowserManager() {
+        properties = new Properties();
+        Path configPath = Paths.get(System.getProperty("config.path",
+                Paths.get(System.getProperty("user.dir"), "src", "main", "resources", "config.properties").toString()));
+        try(InputStream input= Files.newInputStream(configPath)) {
+            properties.load(input);
+        } catch(IOException e) {
+            logger.log(Level.SEVERE, "Failed to load properties file", e);
+        }
+    }
 
     public void setUp() {
-        System.out.println("Setting up Playwright....");
+        logger.info("Setting up Playwright....");
+        String browserType = properties.getProperty("browser", "chromium");
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         int width = (int) screenSize.getWidth();
         int height = (int) screenSize.getHeight();
 
         Playwright playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        switch (browserType.toUpperCase()) {
+            case "CHROMIUM":
+                browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+                break;
+            case "FIREFOX":
+                browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(false));
+                break;
+            default:
+                logger.warning("Unsupported browser type: " + browserType + ". Defaulting to chromium");
+                browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+                break;
+        }
         browserContext = browser.newContext(new Browser.NewContextOptions().setViewportSize(width, height));
         page = browserContext.newPage();
         options = new Page.WaitForSelectorOptions().setTimeout(10000);
-        System.out.println("Playwright setup complete!");
+        logger.info("Playwright setup complete!");
     }
 
     public void tearDown() {
-        System.out.println("Tearing down Playwright...");
+        logger.info("Tearing down Playwright...");
         if (page != null) page.close();
         if (browserContext != null) browserContext.close();
         if (browser != null) browser.close();
         if (playwright != null) playwright.close();
-        System.out.println("Playwright teardown complete!");
+        logger.info("Playwright teardown complete!");
     }
 
     public byte[] takeScreenshot() {
         if (page != null) {
-            return page.screenshot();
+            return page.screenshot((new Page.ScreenshotOptions().setFullPage(true)));
         }
-        return byte[0];
+        return new byte[0];
     }
 }
--- src/test/java/step_definitions/hooks/Hooks.java
+++ src/test/java/step_definitions/hooks/Hooks.java
@@ -3,6 +3,8 @@ package step_definitions.hooks;
 import browser.BrowserManager;
 import io.cucumber.java.*;
 
+import java.util.Collection;
+
 public class Hooks {
     private final BrowserManager browserManager;
 
@@ -24,8 +26,17 @@ public class Hooks {
     @After
     public void teardown(Scenario scenario) {
         if(scenario.isFailed()) {
+            // Get the line number (helpful for Scenario Outlines)
+            Integer lineNumber = scenario.getLine();
+            scenario.log("Failed at line: " + lineNumber);
+            
+            // Get the tags as a Collection of Strings
+            Collection<String> tags = scenario.getSourceTagNames();
+            scenario.log("Scenario Tags: " + tags.toString());
+
+            String fileName = "Failed_" + scenario.getName().replaceAll(" ", "_");
             byte[] screenshot = browserManager.takeScreenshot();
-            scenario.attach(screenshot, "image/png", scenario.getName());
+            scenario.attach(screenshot, "image/png", fileName);
         }
         System.out.println("\nFinished executing the test case!");
         browserManager.tearDown();
